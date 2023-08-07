232dc9c7232bdc465006fde42142b557d29e5725
Ignacio Vassallo <vassallo1994gmail.com>
2023-08-07 14:16:02 -0300

    Creacion de proyecto

---
+++ src/App.jsx
@@ -0,0 +1,32 @@
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import NavBar from "./components/NavBar.jsx";
+import HomePage from "./pages/HomePage";
+import CitiesPage from "./pages/CitiesPage";
+import LoginPage from "./pages/LoginPage";
+import DestinationPage from "./pages/DestinationPage";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <Router>
+        <div >
+          <NavBar />
+          <Routes>
+            <Route path="/" element={<HomePage />} />
+            <Route path="/cities" element={<CitiesPage />} />
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/destination" element={<DestinationPage />} />
+  
+          </Routes>
+        </div>
+      </Router>
+    </>
+  );
+}
+
+export default App;
