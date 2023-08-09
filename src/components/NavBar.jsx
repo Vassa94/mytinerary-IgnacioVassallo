cd8346e48556b1ed4d5eedd21703d56bc925a432
Ignacio Vassallo <vassallo1994gmail.com>
2023-08-09 17:04:38 -0300

    Correcciones en navbar

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -14,18 +14,19 @@ const NavBar = () => {
       className="bg-body-tertiary"
     >
       <Container>
-        <Navbar.Brand to="/">
-          <img className="me-2" src="src/assets/img/logo.png" width="50px" alt="Logo"  />
-          MyTinerary
+        <Navbar.Brand  as={Link} to="/">
+          <img className="me-2" src="src/assets/img/logo.png" width="80px" alt="Logo"  />
+          <span className="text-primary ">My</span>
+          <span >Tinerary</span>
           </Navbar.Brand>
         <Navbar.Toggle aria-controls="responsive-navbar-nav" />
         <Navbar.Collapse id="responsive-navbar-nav">
           <Nav className="me-auto"></Nav>
           <Nav>
-            <Nav.Link as={Link} to="/">Home</Nav.Link>
-            <Nav.Link as={Link} to="cities">Cities</Nav.Link>
-            <Nav.Link as={Link} to="login" className="btn btn-secondary bg-primary ">
-              <i className="fas fa-user"></i> Login
+            <Nav.Link as={Link} to="/"><b>Home</b></Nav.Link>
+            <Nav.Link as={Link} to="cities"><b>Cities</b></Nav.Link>
+            <Nav.Link as={Link} to="login" className="btn btn-secondary bg-primary  ms-2">
+              <i className="fas fa-user"></i> <b>Login</b>
             </Nav.Link>
           </Nav>
         </Navbar.Collapse>
