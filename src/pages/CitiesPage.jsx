fb488085b96b32e7fa39a1ddb8beba12f84db1fb
Ignacio Vassallo <vassallo1994gmail.com>
2023-08-22 22:44:49 -0300

    puesta en uso de cities. eliminado carrousel

--- src/pages/CitiesPage.jsx
+++ src/pages/CitiesPage.jsx
@@ -1,16 +1,57 @@
-/* eslint-disable no-unused-vars */
-import React from "react";
-import Carousel from "../components/Carousel";
+import React, { useState, useEffect } from "react";
+import Card from "../components/Card";
+import NotFound from "../components/NotFound";
 
 const CitiesPage = () => {
+  const [cities, setCities] = useState([]);
+  const [searchTerm, setSearchTerm] = useState("");
+  const [isBannerVisible, setIsBannerVisible] = useState(true);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/cities/view")
+      .then((response) => response.json())
+      .then((data) => setCities(data))
+      .catch((error) => {
+        console.error("Error fetching cities:", error);
+        setCities([]);
+      });
+    console.log(cities);
+  }, []);
+
+  const hideBanner = () => {
+    setIsBannerVisible(false);
+  };
+
+  const filteredCities = cities.filter((cities) =>
+    cities.city.toLowerCase().startsWith(searchTerm.toLowerCase())
+  );
+
   return (
-    <div>
-      <div className="banner-image"></div>
+    <div className={`cities-body ${isBannerVisible ? "" : "fade-out-banner"}`}>
+      <div
+        className={`banner-image ${isBannerVisible ? "" : "fade-out"}`}
+        id="citiesBanner"
+      ></div>
       <div className="search-box">
-        <input type="text" placeholder="Search cities..." />
+        <input
+          type="text"
+          placeholder="Search cities..."
+          value={searchTerm}
+          onChange={(e) => setSearchTerm(e.target.value)}
+          onClick={hideBanner}
+        />
       </div>
       <div className="cities-container">
-        <h3>Coming soon</h3>
+        <h3>Featured cities</h3>
+        {filteredCities.length === 0 ? (
+          <NotFound /> 
+        ) : (
+          <div className="cities-grid">
+            {filteredCities.map((city) => (
+              <Card key={city.id} city={city} />
+            ))}
+          </div>
+        )}
       </div>
     </div>
   );
