9b6faa6ca9d92cd365b2351a7de259ab748de530
Ignacio Vassallo <vassallo1994gmail.com>
2023-09-13 00:29:33 -0300

    Correccion textos

--- src/components/NotFound.jsx
+++ src/components/NotFound.jsx
@@ -3,8 +3,8 @@ import React from "react";
 const NotFound = () => {
   return (
     <div className=" p-5 not-found ">
-      <h2 className="text-center" >No se encontraron ciudades</h2>
-      <p className="text-center" >Lo sentimos, no hay ciudades para mostrar en este momento.</p>
+      <h2 className="text-center" >No cities found</h2>
+      <p className="text-center" >Sorry, there are no cities to show at this time.</p>
       <img src="src/assets/img/404.gif" alt="not-Found" />
     </div>
   );
